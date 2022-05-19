1bcd60e1b1e54d53112d2f8e683d0cca5c34e21f
deguchi37458 <deguchi37458@gmail.com>
2022-05-19 15:12:04 +0900

    update send method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="back">
-    
+    <div class="">
+      <p>送信:</p>
+      <p>{{ sendMessage }}</p>
+      <textarea v-model="message"></textarea>
+      <button @click="sendChat">送信</button>
+    </div>
   </div>
 </template>
 
@@ -8,7 +13,18 @@
 
 
 export default {
-
+  data() {
+    return {
+      message: '',
+      sendMessage: ''
+    }
+  },
+  methods: {
+    sendChat() {
+      this.sendMessage = this.message
+      this.message = ''
+    }
+  }
 }
 </script>
 
